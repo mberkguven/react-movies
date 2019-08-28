20c932dcfca9bee27f7faaabdf6e980c7180f994
Berk Güven <bguven@nuevo.com.tr>
2019-08-28 17:43:51 +0300

    ReduxPromiseMiddleware Added.

--- src/actions/movies.js
+++ src/actions/movies.js
@@ -1,24 +1,20 @@
 import axios from 'axios';
 import {API_BASE} from '../config/env';
 
-export const FETCH_MOVIES = "FETCH_MOVIES";
-export const FETCH_MOVIES_ERROR = "FETCH_MOVIES_ERROR";
+export const FETCH_MOVIES_FULLFILLED = "FETCH_MOVIES_FULLFILLED";
+export const FETCH_MOVIES_REJECTED = "FETCH_MOVIES_ERROR";
 export const ADD_MOVIE = "ADD_MOVIE";
 export const DELETE_MOVIE = "ADD_MOVIE";
 
 
 export function fetchMovies(){
     return dispatch => {
-        axios.get(`${API_BASE}/movies`)
-        .then(result => result.data)
-        .then(data => dispatch({
-            type: FETCH_MOVIES,
-            payload: data.movies
-        }))
-        .catch(error => dispatch({
-            type: FETCH_MOVIES_ERROR,
-            payload: error
-        }))
+       dispatch({
+           type: "FETCH_MOVIES",
+           payload:  axios.get(`${API_BASE}/movies`)
+           .then(result => result.data.movies)
+       })
+
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import * as serviceWorker from './serviceWorker';
 import {createStore,applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
+import reduxPromise from 'redux-promise-middleware';
 import rootReducer from './reducers/rootReducer';
 import {composeWithDevTools} from 'redux-devtools-extension';
 import {Provider} from 'react-redux';
@@ -14,7 +15,7 @@ import { BrowserRouter} from 'react-router-dom';
 
 const store = createStore(
     rootReducer,
-    composeWithDevTools(applyMiddleware(thunk,logger))
+    composeWithDevTools(applyMiddleware(reduxPromise(),thunk,logger))
 );
 
 ReactDOM.render(
